3c3fe96252349162b8105d0a9832b85827fc225c
이재민 <jaemin@MacBookPro.local>
2023-08-25 19:30:09 +0900

    initial commit

---
+++ src/main/java/com/empathday/empathdayapi/EmpathdayApiApplication.java
@@ -0,0 +1,13 @@
+package com.empathday.empathdayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpathdayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpathdayApiApplication.class, args);
+    }
+
+}
