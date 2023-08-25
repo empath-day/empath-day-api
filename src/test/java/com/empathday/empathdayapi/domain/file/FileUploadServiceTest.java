68d4fc34f87620d410e698e30578ae5376e018fd
이재민 <jaemin@172.20.nate.com>
2023-08-25 19:58:50 +0900

    test preSignedUrl

---
+++ src/test/java/com/empathday/empathdayapi/domain/file/FileUploadServiceTest.java
@@ -0,0 +1,35 @@
+package com.empathday.empathdayapi.domain.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.amazonaws.HttpMethod;
+import java.net.URISyntaxException;
+import org.apache.http.client.utils.URIBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadServiceTest {
+
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    @DisplayName("preSignedUrl을 생성합니다.")
+    @Test
+    void generatePreSignedUrl() throws URISyntaxException {
+        // given
+        String filename = "upload.jpg";
+
+        // when
+        String preSignedUrl = fileUploadService.generatePreSignedUrl(filename, HttpMethod.PUT);
+
+        // then
+        URIBuilder uriBuilder = new URIBuilder(preSignedUrl);
+
+        assertThat(preSignedUrl).isNotBlank();
+        assertThat(uriBuilder.getHost()).isEqualTo("empathy-day-img-bucket.s3.ap-northeast-2.amazonaws.com");
+        assertThat(uriBuilder.getPathSegments().get(0)).isEqualTo(filename);
+    }
+}
