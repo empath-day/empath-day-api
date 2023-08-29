b8a73c59b5fb7ad42642898fee624a653ea87c46
jaeminLee524 <rkwoals524@hanmail.net>
2023-08-29 11:50:01 +0900

    add deploy script

--- src/test/java/com/empathday/empathdayapi/api/controller/schedule/ScheduleControllerTest.java
+++ src/test/java/com/empathday/empathdayapi/api/controller/schedule/ScheduleControllerTest.java
@@ -1,19 +1,20 @@
 package com.empathday.empathdayapi.api.controller.schedule;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.empathday.empathdayapi.common.response.CommonResponse.Result;
 import com.empathday.empathdayapi.domain.schedule.ScheduleService;
 import com.empathday.empathdayapi.domain.schedule.emotion.Emotion;
+import com.empathday.empathdayapi.global.config.SecurityConfig;
 import com.empathday.empathdayapi.interfaces.schedule.ScheduleController;
 import com.empathday.empathdayapi.interfaces.schedule.ScheduleDto.RegisterScheduleRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.List;
+import javax.servlet.Filter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -31,7 +33,6 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 })
 public class ScheduleControllerTest {
 
-//    @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
@@ -67,7 +68,6 @@ public class ScheduleControllerTest {
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(MediaType.APPLICATION_JSON)
             )
-            .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.result").value("SUCCESS"))
             .andExpect(jsonPath("$.data").value("OK"))
