6e99b34c950a5c5318edbd4f11afde2b29cbc31b
jaeminLee524 <rkwoals524@hanmail.net>
2023-09-04 13:50:24 +0900

    create swagger controller doc

--- src/main/java/com/empathday/empathdayapi/interfaces/schedule/ScheduleController.java
+++ src/main/java/com/empathday/empathdayapi/interfaces/schedule/ScheduleController.java
@@ -5,6 +5,7 @@ import static com.empathday.empathdayapi.common.response.CommonResponse.success;
 import com.empathday.empathdayapi.common.response.CommonResponse;
 import com.empathday.empathdayapi.domain.schedule.ScheduleService;
 import com.empathday.empathdayapi.interfaces.schedule.ScheduleDto.RegisterScheduleRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,10 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    @Operation(
+        summary = "스케줄 등록",
+        description = "스케줄을 등록할 수 있습니다."
+    )
     @PostMapping("/api/v1/schedules")
     public CommonResponse<String> createSchedule(
         @RequestBody RegisterScheduleRequest request
@@ -30,6 +35,10 @@ public class ScheduleController {
         return success("OK");
     }
 
+    @Operation(
+        summary = "스케줄 조회",
+        description = "일일 단위의 스케줄 정보를 조회할 수 있습니다."
+    )
     @GetMapping("/api/v1/schedules/{id}")
     public CommonResponse retrieveScheduleDetail(
         @PathVariable("id") Long id
--- src/main/java/com/empathday/empathdayapi/interfaces/test/HealCheckController.java
+++ src/main/java/com/empathday/empathdayapi/interfaces/test/HealCheckController.java
@@ -1,11 +1,16 @@
 package com.empathday.empathdayapi.interfaces.test;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealCheckController {
 
+    @Operation(
+        summary = "헬스 체크",
+        description = "일일 단위의 스케줄 정보를 조회할 수 있습니다."
+    )
     @GetMapping("/api/v1/health-check")
     public String healthCheck() {
         return "ok";
